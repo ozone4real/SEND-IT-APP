dec05c2498ec23fe04768b15eede1b4b71b097a1
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-07 18:21:50 +0100

    [fixes #161758918] More preparations for API development

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-const express = require('express');
+import express from 'express';
+import orders from './routes/orders';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1', orders);
+
+const { port } = process.env;
+app.listen(port, () => { console.log(`listening on port ${port}....`); });
+export default app;
+
