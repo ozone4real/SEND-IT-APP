cef57118b818f8d64cc31505f5e566280c4fba5a
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-08 10:43:45 +0100

    [fixes #161758918] More preparations for API development

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,10 +1,10 @@
 import { Router, json, urlencoded } from 'express';
 import parcelData from '../db/parcelData';
-import validateOrder from '../middlewares/validator';
 
 const router = Router();
 
 router.use(json());
 router.use(urlencoded({ extended: true }));
+
 
 export default router;
