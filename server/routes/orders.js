fae418047c2468538d0efb85eb03a6c3b1e52ef7
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-08 07:30:37 +0100

    [finishes #161754643] created and unit tested get endpoint to fetch specific delivery order

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,18 +1,16 @@
 import { Router, json, urlencoded } from 'express';
 import parcelData from '../db/parcelData';
-import validateOrder from '../middlewares/validator';
 
 const router = Router();
 
 router.use(json());
 router.use(urlencoded({ extended: true }));
 
-router.post('/parcels', validateOrder, (req, res) => {
-  const order = req.body;
-  order.id = parcelData.length + 1;
-  order.status = 'received';
-  parcelData.push(order);
-  res.status(200).send(order);
+router.get('/parcels/:parcelId', (req, res) => {
+  const { parcelId } = req.params;
+  const order = parcelData.find(a => a.parcelId === parseInt(parcelId));
+  if (!order) return res.status(404).send('Order not found');
+  res.status(200).json(order);
 });
 
 export default router;
