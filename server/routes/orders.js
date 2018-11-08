bd72897c05ddb7a2991d5d05739086f68960f333
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-08 07:47:49 +0100

    [fixes #161758918] More preparations for API development

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -7,12 +7,4 @@ const router = Router();
 router.use(json());
 router.use(urlencoded({ extended: true }));
 
-router.post('/parcels', validateOrder, (req, res) => {
-  const order = req.body;
-  order.id = parcelData.length + 1;
-  order.status = 'received';
-  parcelData.push(order);
-  res.status(200).send(order);
-});
-
 export default router;
