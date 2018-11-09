c33eabeca5702efe8657dca9c01ad6f8abd4a32b
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-09 09:29:08 +0100

    [finishes #161835092] restructuring of modules after successful merge of feature branches

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,12 +1,22 @@
 import { Router, json, urlencoded } from 'express';
 import parcelData from '../db/parcelData';
+import validateOrder from '../middlewares/validator';
 
 const router = Router();
 
 router.use(json());
 router.use(urlencoded({ extended: true }));
 
-router.get('/parcels/:parcelId', (req, res) => {
+router.post('/', validateOrder, (req, res) => {
+  const order = req.body;
+  order.id = parcelData.length + 1;
+  order.status = 'received';
+  parcelData.push(order);
+  res.status(200).send(order);
+});
+
+
+router.get('/:parcelId', (req, res) => {
   const { parcelId } = req.params;
   const order = parcelData.find(a => a.parcelId === parseInt(parcelId));
   if (!order) return res.status(404).send('Order not found');
