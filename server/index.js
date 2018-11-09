53607bb9194f6261940149ab2b64fdea86553966
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-09 16:42:40 +0100

    modified package.json file

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use('/api/v1/parcels', orders);
 app.use('/api/v1/users', users);
 
-const { port } = process.env;
+const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`listening on port ${port}....`); });
 
 export default app;
