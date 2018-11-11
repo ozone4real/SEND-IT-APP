d730855287b78b1706b5e75b0b170e154d03bccc
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-11 19:03:57 +0100

    refactored validator middleware

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -1,21 +1,7 @@
 /* eslint-disable consistent-return */
-function validator(req, res, next) {
-
-  const dataKeys = ['userId', 'pickupAddress', 'deliveryAddress', 'deliveryTime', 'parcelDescription'];
-
-  let missingKeys = [];
-
-  dataKeys.forEach((item) => {
-    if (!req.body[item]) missingKeys.push(item);
-  });
-
-  if (missingKeys.length !== 0) {
-    missingKeys = missingKeys.join(', ');
-    return res.status(400).json({ message: `Incomplete request: ${missingKeys} parameters are missing` });
-  }
-
+function improperVal(req, res) {
   const {
-    userId, pickupAddress, deliveryAddress, deliveryTime, parcelDescription,
+    pickupAddress, deliveryAddress, deliveryTime, parcelDescription,
   } = req.body;
 
   const timeTest = /^\d{4}-((1[0-2])|(0[1-9]))-((3[0-1])|([0-2][1-9]))T(([0-1][0-9])|(2[0-3])):[0-5][0-9]$/.test(deliveryTime);
@@ -34,8 +20,26 @@ function validator(req, res, next) {
     improperValues = improperValues.join(', ');
     return res.status(400).json({ message: improperValues });
   }
+}
 
+
+function validator(req, res, next) {
+  const dataKeys = ['userId', 'pickupAddress', 'deliveryAddress', 'deliveryTime', 'parcelDescription'];
+
+  let missingKeys = [];
+
+  dataKeys.forEach((item) => {
+    if (!req.body[item]) missingKeys.push(item);
+  });
+
+  if (missingKeys.length !== 0) {
+    missingKeys = missingKeys.join(', ');
+    return res.status(400).json({ message: `Incomplete request: ${missingKeys} parameters are missing` });
+  }
+
+  improperVal(req, res);
   next();
 }
+
 
 export default validator;
