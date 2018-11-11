0346945d317092e7dd56bbea93abd2110ee66779
ozone4real <ezenwaogbonna1.@gmail.com>
2018-11-11 19:30:13 +0100

    refactored validation middleware

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -1,5 +1,5 @@
 /* eslint-disable consistent-return */
-function improperVal(req, res) {
+function improperVal(req) {
   const {
     pickupAddress, deliveryAddress, deliveryTime, parcelDescription,
   } = req.body;
@@ -9,37 +9,42 @@ function improperVal(req, res) {
   const dAdTest = /.{15,}/.test(deliveryAddress);
   const pDeTest = /.{10,40}/.test(parcelDescription);
 
-  let improperValues = [];
+  const improperValues = [];
 
   if (!timeTest) improperValues.push('Improper date-time format or invalid date. Pattern should follow: yyyy:mm:ddThh:mm');
   if (!pAdTest) improperValues.push('pickupAddress not detailed enough');
   if (!dAdTest) improperValues.push('deliveryAddress not detailed enough');
   if (!pDeTest) improperValues.push('parcelDescription not detailed enough or too long. Min. Length:15, Max. Length:40');
 
-  if (improperValues.length !== 0) {
-    improperValues = improperValues.join(', ');
-    return res.status(400).json({ message: improperValues });
-  }
+  return improperValues;
 }
 
-
-function validator(req, res, next) {
+function incompleteVal(req) {
   const dataKeys = ['userId', 'pickupAddress', 'deliveryAddress', 'deliveryTime', 'parcelDescription'];
-
-  let missingKeys = [];
+  const missingKeys = [];
 
   dataKeys.forEach((item) => {
     if (!req.body[item]) missingKeys.push(item);
   });
+
+  return missingKeys;
+}
+
+function validator(req, res, next) {
+  let missingKeys = incompleteVal(req);
 
   if (missingKeys.length !== 0) {
     missingKeys = missingKeys.join(', ');
     return res.status(400).json({ message: `Incomplete request: ${missingKeys} parameters are missing` });
   }
 
-  improperVal(req, res);
+  let improperValues = improperVal(req);
+
+  if (improperValues.length !== 0) {
+    improperValues = improperValues.join(', ');
+    return res.status(400).json({ message: improperValues });
+  }
   next();
 }
-
 
 export default validator;
