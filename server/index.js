fed823e8f869aaf69466558119061f5e9af4f8f4
ozone4real <ezenwaogbonna1.@gmail.com>
2019-01-04 08:33:20 +0100

    modifies routes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import router from './routes';
-import createTables from './db/migrations';
-import error from './helpers/error';
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+import router from "./routes";
+import createTables from "./db/migrations";
+import error from "./middlewares/error";
 
-const swaggerDocument = YAML.load('./server/docs/docs.yaml');
+const swaggerDocument = YAML.load("./server/docs/docs.yaml");
 const app = express();
 
 createTables();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.static('UI'));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.static("UI"));
 
-app.use('/api/v1/', router);
+app.use("/api/v1/", router);
+app.all("*", (req, res) => {
+  res.status(404).json({
+    message: "Invalid route. Please check that the link is correct?"
+  });
+});
 app.use(error);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => { console.log(`listening on port ${port}....`); });
+app.listen(port, () => {
+  console.log(`listening on port ${port}....`);
+});
 
 export default app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,5 @@ const router = Router();
 router.use('/parcels', parcels);
 router.use('/user', user);
 router.use('/auth', auth);
-router.all('*', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to SENDIT. How are you doing?'
-  });
-});
 
 export default router;
